6d40e7ab32857fe787363fb9e2c629f0804d1fe4
rdityaa <radityarizki2309@gmail.com>
2025-02-02 17:06:39 +0700

    update credit

--- Credit.java
+++ Credit.java
@@ -17,6 +17,14 @@ public class Credit extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1);
-        addObject(new HomeButton(), getWidth() / 2, 700);
+        addObject(new HomeButton(), getWidth() / 2, 500);
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
